0b989788b5749ee8f2edea5bb21a0f7df75062bc
Hardyson Arthy Robin - <hardysonarthy@airasia.com>
2024-11-13 10:06:18 +0800

    copy draft

--- app/tools/jsontotable/page.tsx
+++ app/tools/jsontotable/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { ChangeEvent, ChangeEventHandler, useRef, useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
@@ -14,17 +13,20 @@ import { Button } from '@/components/ui/button';
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { AlertCircle } from 'lucide-react';
 
 export default function JsonToTable() {
   const [rows, setRows] = useState([]);
   const [table, setTable] = useState([]);
   const [maxCol, setMaxCol] = useState(0);
+  const [enableConvert, setEnableConvert] = useState(false);
+  const [jsonAlert, setJsonAlert] = useState(false);
   const tableRef = useRef(null);
 
   let maxCount = 0;
@@ -54,9 +56,23 @@ export default function JsonToTable() {
       }
 
       if (typeof value === 'object') {
+        if (value === null) {
+          genRows.push({
+            name: key,
+            valueType: 'null',
+          });
+          continue;
+        }
+        if (value === undefined) {
+          genRows.push({
+            name: key,
+            valueType: 'undefined',
+          });
+          continue;
+        }
         genRows.push({
           name: key,
-          valueType: 'Object[]',
+          valueType: 'Object',
           child: convertJson(value, nestCount + 1),
         });
         continue;
@@ -76,17 +92,21 @@ export default function JsonToTable() {
   function renderRow(row, parentName, level = 0) {
     if (level === 0) {
       return (
-        <TableRow key={`row-${row.name}`}>
+        <TableRow className="border" key={`row-${row.name}`}>
           <TableCell
+            className="border"
             key={`row-${row.name}-name`}
             colSpan={maxCol === 0 ? undefined : maxCol + 1}
           >
             {row.name}
           </TableCell>
-          <TableCell key={`row-${row.name}-valueType`}>
+          <TableCell className="border" key={`row-${row.name}-valueType`}>
             {row.valueType}
           </TableCell>
-          <TableCell key={`row-${row.name}-description`}></TableCell>
+          <TableCell
+            className="border"
+            key={`row-${row.name}-description`}
+          ></TableCell>
         </TableRow>
       );
     }
@@ -100,15 +120,20 @@ export default function JsonToTable() {
       <TableRow key={`row-${parentName}-${row.name}-${level}`}>
         {prependTableCell}
         <TableCell
+          className="border"
           key={`row-${parentName}-${row.name}-name-${level}`}
           colSpan={maxCol + 1 - level}
         >
           {row.name}
         </TableCell>
-        <TableCell key={`row-${parentName}-${row.name}-valueType-${level}`}>
+        <TableCell
+          className="border"
+          key={`row-${parentName}-${row.name}-valueType-${level}`}
+        >
           {row.valueType}
         </TableCell>
         <TableCell
+          className="border"
           key={`row-${parentName}-${row.name}-description-${level}`}
         ></TableCell>
       </TableRow>
@@ -138,21 +163,36 @@ export default function JsonToTable() {
 
   function copyTableHTML() {
     const copiableTable = tableRef.current.innerHTML;
-    // const textToCopy = copiableTable.map((row) => row).join('');
-    // console.log(textToCopy);
     navigator.clipboard.writeText(copiableTable);
+  }
+
+  function generateChildRows(newRow, row) {
+    if (row.child && row.child.length > 0) {
+      const newRowChild = row.child.map((child) => {
+        if (child.child) {
+          return generateChildRows(newRow, child);
+        }
+        return [child.name, child.valueType].join(' ');
+      });
+      console.log(newRowChild);
+      newRow = [newRow, newRowChild.join('\n')].join('\n');
+    }
+    return newRow;
   }
 
   function copyTable() {}
 
   function onChange(event: ChangeEvent<HTMLTextAreaElement>) {
     try {
+      setJsonAlert(false);
       const text = event.target.value;
       const { genRows, maxCount } = convertJson(JSON.parse(text));
       setRows(genRows);
       setMaxCol(maxCount);
+      setEnableConvert(true);
     } catch (error) {
-      console.log('error');
+      setJsonAlert(true);
+      setEnableConvert(false);
     }
   }
 
@@ -164,57 +204,74 @@ export default function JsonToTable() {
           <CardDescription>Input your JSON file here</CardDescription>
         </CardHeader>
         <CardContent>
+          {jsonAlert && (
+            <Alert variant="destructive">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Unable to parse JSON!</AlertTitle>
+              <AlertDescription>
+                Please check your JSON formatting.
+              </AlertDescription>
+            </Alert>
+          )}
           <Textarea
             id="jsonText"
             onChange={onChange}
-            className="w-full min-h-80"
+            className="w-full min-h-80 mt-2"
           ></Textarea>
-          <Button className="button w-full mt-2" onClick={() => onClick()}>
+          <Button
+            className="button w-full mt-2"
+            disabled={!enableConvert}
+            onClick={() => onClick()}
+          >
             Convert
           </Button>
         </CardContent>
       </Card>
-      <Card className="b h-max">
-        <CardHeader>
-          <CardTitle className="flex justify-between">
-            <div className="">Output Table {maxCol}</div>
-            <div className="space-x-1">
-              <Button
-                variant="ghost"
-                className="text-sm h-8 px-4"
-                disabled={table.length === 0}
-                onClick={copyTableHTML}
-              >
-                Copy HTML Table
-              </Button>
+      {table.length > 0 && (
+        <Card className="b h-max">
+          <CardHeader>
+            <CardTitle className="flex justify-between">
+              <div className="">Output Table</div>
+              <div className="space-x-1">
+                <Button
+                  variant="ghost"
+                  className="text-sm h-8 px-4"
+                  disabled={table.length === 0}
+                  onClick={copyTableHTML}
+                >
+                  Copy HTML Table
+                </Button>
 
-              <Button
-                variant="ghost"
-                className="text-sm h-8 px-4"
-                disabled={table.length === 0}
-                onClick={copyTable}
-              >
-                Copy Raw Table
-              </Button>
-            </div>
-          </CardTitle>
-          <CardDescription>Table output</CardDescription>
-        </CardHeader>
-        <CardContent id="tableRender">
-          <Table ref={tableRef}>
-            <TableHeader>
-              <TableRow>
-                <TableHead colSpan={maxCol === 0 ? undefined : maxCol + 1}>
-                  Key
-                </TableHead>
-                <TableHead>Type</TableHead>
-                <TableHead>Description</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>{table}</TableBody>
-          </Table>
-        </CardContent>
-      </Card>
+                <Button
+                  variant="ghost"
+                  className="text-sm h-8 px-4"
+                  disabled={true} //{table.length === 0}
+                  onClick={copyTable}
+                >
+                  Copy Raw Table (Not available)
+                </Button>
+              </div>
+            </CardTitle>
+          </CardHeader>
+          <CardContent id="tableRender">
+            <Table ref={tableRef} className="border border-collapse">
+              <TableHeader>
+                <TableRow>
+                  <TableHead
+                    className="border"
+                    colSpan={maxCol === 0 ? undefined : maxCol + 1}
+                  >
+                    Key
+                  </TableHead>
+                  <TableHead className="border">Type</TableHead>
+                  <TableHead className="border">Description</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>{table}</TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
